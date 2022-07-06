07b1c89d80e5e8d00c75b01f3527645c016ad88a
davidpasek <david.pasek@gmail.com>
2022-07-06 20:01:53 +0000

    Output formating

--- python/function-latency.py
+++ python/function-latency.py
@@ -17,7 +17,7 @@ def main():
 
   print ("Test latency of function call")
   t1 = time.time_ns()
-  print ("Start time: ",t1)
+  print ("Start time:",t1)
 
   num_of_func_calls = 10000000
   i = num_of_func_calls
@@ -32,9 +32,9 @@ def main():
 
   t = t2 - t1
   print ("Number of function calls: ",num_of_func_calls)
-  print ("Process time: ",t," ns")
+  print ("Process time:",t,"ns")
   ft = t / num_of_func_calls
-  print ("Single function time: ", ft); 
+  print ("Single function time:", ft, "ns"); 
 
 # Main
 main()
